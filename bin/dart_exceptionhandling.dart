016acdbad289126f6ab582381cdab52484ecd743
Pedro Henrique <pedrohbc96@gmail.com>
2023-09-05 19:48:32 -0300

    feat: subindo arquivos iniciais

---
+++ bin/dart_exceptionhandling.dart
@@ -0,0 +1,5 @@
+import 'package:dart_exceptionhandling/dart_exceptionhandling.dart' as dart_exceptionhandling;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_exceptionhandling.calculate()}!');
+}
---
+++ test/dart_exceptionhandling_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_exceptionhandling/dart_exceptionhandling.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
